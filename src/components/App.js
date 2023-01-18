5ea0e5b5431f9a132007c25fd82e89cde795019d
mbugua <mbuguafaimba@gmail.com>
2023-01-18 09:22:56 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React from "react";
+import { useEffect,useState } from "react";
+
+const DogFetch = () => {
+    const [data,setData] = useState([])
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res=>res.json())
+        .then((response)=>{
+            setData(response.message)
+            
+        })
+    },[])
+    if (!data) {
+        return <p>Loading...</p>
+    }
+    return (
+        <>
+            <img src={data} alt="" />
+        </>
+    );
+}
+ 
+export default DogFetch;
